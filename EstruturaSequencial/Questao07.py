a19aacb605d49668ec775a89f0533717026ed676
Gabriel Amorim <therealamorim@gmail.com>
2021-08-31 19:37:34 -0300

    fix: questao 07 using conventions

--- EstruturaSequencial/Questao07.py
+++ EstruturaSequencial/Questao07.py
@@ -2,6 +2,6 @@ lado = float(input("Digite o valor do lado de um quadrado: "))
 
 area = pow(lado, 2)
 
-dobroArea = area * 2
+dobro_area = area * 2
 
-print("O dobro da área desse quadrado é: ", dobroArea)
+print("O dobro da área desse quadrado é: ", dobro_area)
