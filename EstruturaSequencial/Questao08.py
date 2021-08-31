e366cdb86112f7f41ce1dc867cb843f6e293d9fe
Gabriel Amorim <therealamorim@gmail.com>
2021-08-31 19:37:42 -0300

    feat: questao 08

---
+++ EstruturaSequencial/Questao08.py
@@ -0,0 +1,7 @@
+valor_da_hora = float(input("Digite o valor da hora: "))
+
+horas_trabalhadas_mes = float(input("Digite o número de horas trabalhadas no mês: "))
+
+salario = valor_da_hora * horas_trabalhadas_mes
+
+print("Total do salário: ", salario)
