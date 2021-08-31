8b5c771e4dccb5f357a3c4b0d542bf7ef5b021c8
Gabriel Amorim <therealamorim@gmail.com>
2021-08-31 19:40:34 -0300

    feat: questao 09

---
+++ EstruturaSequencial/Questao09.py
@@ -0,0 +1,5 @@
+graus_fahrenheit = float(input("Digite a temperatura em graus Fahrenheit: "))
+
+graus_celsius = 5 * (graus_fahrenheit - 32) / 9
+
+print("Temperatura em graus Celsius: %.2f" %graus_celsius)
