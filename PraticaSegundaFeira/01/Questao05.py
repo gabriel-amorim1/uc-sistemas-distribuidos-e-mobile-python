20bc43cf97d9e31b70d1becfce99c21dbc7170f6
Gabriel Amorim <therealamorim@gmail.com>
2021-09-13 21:43:49 -0300

    fix: correcao questao 05

--- PraticaSegundaFeira/01/Questao05.py
+++ PraticaSegundaFeira/01/Questao05.py
@@ -4,7 +4,7 @@ r = requests.get('http://www.google.com/search',
                  params={'q': 'gabriel de amorim'})
 
 if (r.status_code == 200):
-    arquivo = open("resultados.txt", "a")
+    arquivo = open("resultados.txt", "w")
     arquivo.write(r.text)
 else:
     print('Nao houve sucesso na requisicao.')
