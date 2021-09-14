a9e65a2e89471e8ad0d8b2392cb249b35b226164
Gabriel Amorim <therealamorim@gmail.com>
2021-09-13 21:44:35 -0300

    fix: correcao questao 05

--- PraticaSegundaFeira/01/Questao05.py
+++ PraticaSegundaFeira/01/Questao05.py
@@ -6,5 +6,6 @@ r = requests.get('http://www.google.com/search',
 if (r.status_code == 200):
     arquivo = open("resultados.txt", "w")
     arquivo.write(r.text)
+    arquivo.close
 else:
     print('Nao houve sucesso na requisicao.')
